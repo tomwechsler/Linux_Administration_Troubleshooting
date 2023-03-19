e11240d95d0b17f4494fbc962b228ca7afbe0f42
Tom Wechsler <tom.wechsler@outlook.com>
2023-03-19 15:19:02 +0100

    new files

---
+++ A03/01_Installing_sysstat.sh
@@ -0,0 +1,20 @@
+#Working on ubuntu
+
+#Installing sysstat
+sudp apt install -y sysstat
+
+#Start the service
+sudo systemctl enable --now sysstat
+
+sudo systemctl status sysstat
+
+#Sysstat is gathering information
+cat /etc/cron.d/sysstat
+
+#But is not enabled
+sudo vim /etc/default/sysstat
+
+#Change to true
+
+#Save and exit
+
